b842f053d7943bd829c94609038568be8097327a
codefaring <codefaring@gmail.com>
2022-12-31 23:49:30 +0900

    state test

---
+++ src/components/Count.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+export default function Count() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className='count'>
+      <span className='num'>{count}</span>
+      <p>
+        <button className='button' onClick={() => {setCount(count + 1)}}>+ 1</button>
+        <button className='button' onClick={() => {setCount(count + 5)}}>+ 5</button>
+        <button className='button' onClick={() => {setCount(count + 10)}}>+ 10</button>
+      </p>
+      <p>
+        <button className='button' onClick={() => {setCount(count - 1)}}>- 1</button>
+        <button className='button' onClick={() => {
+          setCount((prev) => prev -1);
+          setCount((prev) => prev -1);
+          setCount((prev) => prev -1);
+          setCount((prev) => prev -1);
+          setCount((prev) => prev -1);
+        }}>- 5</button>
+        <button className='button' onClick={() => {setCount(count - 10)}}>- 10</button>
+      </p>
+    </div>
+  )
+}
