bd5a3c9db8328586c2ca97dcc241d8dcbc3e7808
codefaring <121625285+codefaring@users.noreply.github.com>
2023-01-01 12:28:30 +0900

    Update Count.jsx

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -3,24 +3,43 @@ import React, { useState } from 'react'
 export default function Count() {
   const [count, setCount] = useState(0);
   return (
-    <div className='count'>
-      <span className='num'>{count}</span>
-      <p>
-        <button className='button' onClick={() => {setCount(count + 1)}}>+ 1</button>
-        <button className='button' onClick={() => {setCount(count + 5)}}>+ 5</button>
-        <button className='button' onClick={() => {setCount(count + 10)}}>+ 10</button>
-      </p>
-      <p>
-        <button className='button' onClick={() => {setCount(count - 1)}}>- 1</button>
-        <button className='button' onClick={() => {
-          setCount((prev) => prev -1);
-          setCount((prev) => prev -1);
-          setCount((prev) => prev -1);
-          setCount((prev) => prev -1);
-          setCount((prev) => prev -1);
-        }}>- 5</button>
-        <button className='button' onClick={() => {setCount(count - 10)}}>- 10</button>
-      </p>
-    </div>
-  )
+      <div className='counter'>
+        <span className='number'>{count}</span>
+        <p>
+          <button className='button' onClick={() => {
+            setCount(count + 1);
+          }}>+ 1</button>
+          <button className='button' onClick={() => {
+            setCount(count + 5);
+          }}>+ 5</button>
+          <button className='button' onClick={() => {
+            setCount(count + 10);
+          }}>+ 10</button>
+        </p>
+        <p>
+          <button className='button' onClick={() => {
+            setCount((prev) => prev -1);
+          }}>- 1</button>
+          <button className='button' onClick={() => {
+            setCount((prev) => prev -1);
+            setCount((prev) => prev -1);
+            setCount((prev) => prev -1);
+            setCount((prev) => prev -1);
+            setCount((prev) => prev -1);
+          }}>- 5</button>
+          <button className='button' onClick={() => {
+            setCount((prev) => prev -1);
+            setCount((prev) => prev -1);
+            setCount((prev) => prev -1);
+            setCount((prev) => prev -1);
+            setCount((prev) => prev -1);
+            setCount((prev) => prev -1);
+            setCount((prev) => prev -1);
+            setCount((prev) => prev -1);
+            setCount((prev) => prev -1);
+            setCount((prev) => prev -1);
+          }}>- 10</button>
+        </p>
+      </div>
+  );
 }
